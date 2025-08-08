9b2ac1847334b59701459b18b8f3852c66ab9de1
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-08 18:40:56 +0530

    Add code

---
+++ src/main/java/com/Simple/Blog/Backend/Simple/Blog/Backend/Controller/BlogController.java
@@ -0,0 +1,11 @@
+package com.Simple.Blog.Backend.Simple.Blog.Backend.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+
+
+}
