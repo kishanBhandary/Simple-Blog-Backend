ed137d6f6c92cb99dfeffb9cf3f743ff955ff40c
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-08 18:29:33 +0530

    Add code

---
+++ src/main/java/com/Simple/Blog/Backend/Simple/Blog/Backend/SimpleBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Simple.Blog.Backend.Simple.Blog.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Simple/Blog/Backend/Simple/Blog/Backend/SimpleBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Simple.Blog.Backend.Simple.Blog.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
