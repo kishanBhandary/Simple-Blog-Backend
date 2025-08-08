6675a4c755586c7ce6b6fd9e202e965e460a152d
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-08 18:34:46 +0530

    Add code

--- src/main/java/com/Simple/Blog/Backend/Simple/Blog/Backend/Model/Blog.java
+++ src/main/java/com/Simple/Blog/Backend/Simple/Blog/Backend/Model/Blog.java
@@ -1,9 +1,17 @@
 package com.Simple.Blog.Backend.Simple.Blog.Backend.Model;
 
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Blog {
-
+    @NonNull
     private String title;
+    @NonNull
     private String content;
+    @NonNull
     private String author;
-
 }
