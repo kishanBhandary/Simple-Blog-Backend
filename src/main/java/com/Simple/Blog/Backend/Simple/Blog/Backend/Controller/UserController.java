7d6417e34fab50960410c2dc3259f99033449304
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-16 13:38:09 +0530

    Add code

--- src/main/java/com/Simple/Blog/Backend/Simple/Blog/Backend/Controller/UserController.java
+++ src/main/java/com/Simple/Blog/Backend/Simple/Blog/Backend/Controller/UserController.java
@@ -1,10 +1,17 @@
 package com.Simple.Blog.Backend.Simple.Blog.Backend.Controller;
 
+import com.Simple.Blog.Backend.Simple.Blog.Backend.Model.User;
+import com.Simple.Blog.Backend.Simple.Blog.Backend.Service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+     private  UserService userService;
+    @PostMapping
+        public void addUser(User user){
 
+        }
 }
